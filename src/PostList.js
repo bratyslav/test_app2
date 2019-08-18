9f5f43c6dfd7dce3713dffffec52fba476a00771
bratyslav <beginner.altavista@gmail.com>
2019-08-18 17:52:53 +0300

    added ability to add post

--- src/PostList.js
+++ src/PostList.js
@@ -1,16 +1,66 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { addPost } from 'store';
+import { addPost } from './store';
 import { NavLink } from 'react-router-dom';
 
 class PostList extends React.Component {
+  state = {
+    post: '',
+    title: '',
+    creator: ''
+  };
+
+  getCurrentDate = () => {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const yyyy = today.getFullYear();
+
+    return `${yyyy}-${mm}-${dd}`;
+  }
+
+  sendPost = (event) => {
+    event.preventDefault();
+    const { post, title, creator } = this.state;
+
+    this.props.addPost({
+      id: title + Math.floor(Math.random()*10),
+      title: title,
+      body: post,
+      creator: creator,
+      comments: [],
+      date: this.getCurrentDate()
+    })
+  };
+
+  recordPostBody = (event) => {
+    this.setState({ post: event.target.value });
+  };
+
+  recordPostTitle = (event) => {
+    this.setState({ title: event.target.value });
+  };
+
+  recordPostCreator = (event) => {
+    this.setState({ creator: event.target.value });
+  };
+
   render() {
+    const { post, title, creator } = this.state;
+
     return (
       this.props.posts ?
 
       <div>
         <h1>Latest Posts</h1>
-  
+
+        <form onSubmit={this.sendPost}>
+          Title: <input type="text" onChange={this.recordPostTitle} value={title} />
+          Author: <input type="text" onChange={this.recordPostCreator} value={creator} />
+          <textarea onChange={this.recordPostBody} value={post} />
+          <input type="submit" value="Submit" />
+        </form>
+
         <ul>
           {
             this.props.posts.map(post => (
